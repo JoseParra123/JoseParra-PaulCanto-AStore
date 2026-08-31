f05cb23b273db1366a2f914358db7f9484a2f1cf
JoseParra123 <jose.parra2007@anahuac.mx>
2026-08-31 12:17:51 -0500

    Create Maquinas.java

---
+++ Maquinas.java
@@ -0,0 +1,20 @@
+public class Maquinas {
+    // Atributos
+    private String ID_Equipo;
+    private String Nombre_Terminal;
+    private String IP_Red;
+    private String Tipo_Maquinaria;
+    private String Estado_Operativo;
+    private String Ubicacion;
+
+    // Métodos
+    public void asignarNombreEquipo() {}
+    public void configurarIP() {}
+    public void iniciarSesion() {}
+    public void reportarFalla() {}
+    public void imprimirTicket() {}
+    public void calibrarLector() {}
+    public void solicitarMantenimiento() {}
+    public void reiniciarTerminal() {}
+    public void actualizarSoftware() {}
+}
