19e58069934d85ebe9c3933d99881e2189b2c4e4
JoseParra123 <jose.parra2007@anahuac.mx>
2026-08-31 12:19:39 -0500

    Create Personal.java

---
+++ Personal.java
@@ -0,0 +1,24 @@
+public class Personal {
+    // Atributos
+    private String ID_Empleado;
+    private String Nombre_Completo;
+    private String CURP_RFC;
+    private String Puesto_Rol;
+    private String Horario_Laboral;
+    private String Contacto_Emergencia;
+    private String Estado_Activo;
+
+    // Métodos
+    public void asignarNombre() {}
+    public void establecerPuesto() {}
+    public void asociarBiometrico() {}
+    public void darDeAlta() {}
+    public void modificarDatos() {}
+    public void darDeBaja() {}
+    public void registrarAsistencia() {}
+    public void asignarCaja() {}
+    public void solicitarPermiso() {}
+    public void generarReporteHoras() {}
+    public void evaluarDesempeno() {}
+    public void cambiarContrasena() {}
+}
