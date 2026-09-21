0bf9dad5b6e320bdb082e76388399601eda773f3
JoseParra123 <jose.parra2007@anahuac.mx>
2026-09-21 11:54:23 -0500

    Update clientes.java

--- clientes.java
+++ clientes.java
@@ -1,14 +1,27 @@
-public class Clientes {
-    public void asignarNombreCliente() {} //[cite: 1]
-    public void vincularCorreoInstitucional() {} //[cite: 1]
-    public void validarCredencialDigital() {} //[cite: 1]
-    public void escanearQRApp() {} //[cite: 1]
-    public void acumularPuntos() {} //[cite: 1]
-    public void redimirPuntos() {} //[cite: 1]
-    public void aplicarDescuentoPerfil() {} //[cite: 1]
-    public void procesarPagoNomina() {} //[cite: 1]
-    public void consultarHistorial() {} //[cite: 1]
-    public void consultarSaldoPuntos() {} //[cite: 1]
-    public void bloquearCuenta() {} //[cite: 1]
-    public void verificarEstatusAcademico() {} //[cite: 1]
+public class clientes extends Persona {
+    private String correoInstitucional;
+    private int puntosLeones;
+    private double creditoInstitucional;
+
+    public clientes(String id, String nombreCompleto, String telefono, String correoInstitucional) {
+        super(id, nombreCompleto, telefono); // Llama al constructor de la clase padre Persona
+        this.correoInstitucional = correoInstitucional;
+        this.puntosLeones = 0;
+        this.creditoInstitucional = 0.0;
+    }
+
+    // Validaciones en Setters (Encapsulamiento)
+    public void setPuntosLeones(int puntosLeones) {
+        if (puntosLeones >= 0) {
+            this.puntosLeones = puntosLeones;
+        } else {
+            System.out.println("Error: Los puntos no pueden ser negativos.");
+        }
+    }
+
+    public int getPuntosLeones() { return puntosLeones; }
+    public String getCorreoInstitucional() { return correoInstitucional; }
+    public void setCorreoInstitucional(String correoInstitucional) { this.correoInstitucional = correoInstitucional; }
+    public double getCreditoInstitucional() { return creditoInstitucional; }
+    public void setCreditoInstitucional(double creditoInstitucional) { this.creditoInstitucional = creditoInstitucional; }
 }
