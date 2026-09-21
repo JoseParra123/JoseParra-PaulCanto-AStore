a86f386d6a3fed2af74a568c90a0a4858f698089
JoseParra123 <jose.parra2007@anahuac.mx>
2026-09-21 08:39:28 -0500

    Update Personal.java

--- Personal.java
+++ Personal.java
@@ -1,5 +1,4 @@
-public class Personal {
-    // Atributos
+public class Personal { //[cite: 9]
     private String ID_Empleado;
     private String Nombre_Completo;
     private String CURP_RFC;
@@ -8,7 +7,21 @@ public class Personal {
     private String Contacto_Emergencia;
     private String Estado_Activo;
 
-    // Métodos
+    public String getID_Empleado() { return ID_Empleado; }
+    public void setID_Empleado(String ID_Empleado) { this.ID_Empleado = ID_Empleado; }
+    public String getNombre_Completo() { return Nombre_Completo; }
+    public void setNombre_Completo(String Nombre_Completo) { this.Nombre_Completo = Nombre_Completo; }
+    public String getCURP_RFC() { return CURP_RFC; }
+    public void setCURP_RFC(String CURP_RFC) { this.CURP_RFC = CURP_RFC; }
+    public String getPuesto_Rol() { return Puesto_Rol; }
+    public void setPuesto_Rol(String Puesto_Rol) { this.Puesto_Rol = Puesto_Rol; }
+    public String getHorario_Laboral() { return Horario_Laboral; }
+    public void setHorario_Laboral(String Horario_Laboral) { this.Horario_Laboral = Horario_Laboral; }
+    public String getContacto_Emergencia() { return Contacto_Emergencia; }
+    public void setContacto_Emergencia(String Contacto_Emergencia) { this.Contacto_Emergencia = Contacto_Emergencia; }
+    public String getEstado_Activo() { return Estado_Activo; }
+    public void setEstado_Activo(String Estado_Activo) { this.Estado_Activo = Estado_Activo; }
+
     public void asignarNombre() {}
     public void establecerPuesto() {}
     public void asociarBiometrico() {}
