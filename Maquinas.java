dfb7b9e6851f48d14b63c8cb1dd36b0f2f4d4513
JoseParra123 <jose.parra2007@anahuac.mx>
2026-09-21 08:37:26 -0500

    Update Maquinas.java

--- Maquinas.java
+++ Maquinas.java
@@ -1,5 +1,4 @@
-public class Maquinas {
-    // Atributos
+public class Maquinas { //[cite: 5, 9]
     private String ID_Equipo;
     private String Nombre_Terminal;
     private String IP_Red;
@@ -7,7 +6,19 @@ public class Maquinas {
     private String Estado_Operativo;
     private String Ubicacion;
 
-    // Métodos
+    public String getID_Equipo() { return ID_Equipo; }
+    public void setID_Equipo(String ID_Equipo) { this.ID_Equipo = ID_Equipo; }
+    public String getNombre_Terminal() { return Nombre_Terminal; }
+    public void setNombre_Terminal(String Nombre_Terminal) { this.Nombre_Terminal = Nombre_Terminal; }
+    public String getIP_Red() { return IP_Red; }
+    public void setIP_Red(String IP_Red) { this.IP_Red = IP_Red; }
+    public String getTipo_Maquinaria() { return Tipo_Maquinaria; }
+    public void setTipo_Maquinaria(String Tipo_Maquinaria) { this.Tipo_Maquinaria = Tipo_Maquinaria; }
+    public String getEstado_Operativo() { return Estado_Operativo; }
+    public void setEstado_Operativo(String Estado_Operativo) { this.Estado_Operativo = Estado_Operativo; }
+    public String getUbicacion() { return Ubicacion; }
+    public void setUbicacion(String Ubicacion) { this.Ubicacion = Ubicacion; }
+
     public void asignarNombreEquipo() {}
     public void configurarIP() {}
     public void iniciarSesion() {}
